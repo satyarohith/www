b472677ffe9cafc4419b65412436bc2a2684c37a
Satya Rohith <me@satyarohith.com>
2022-03-13 22:05:36 +0530

    init

---
+++ routes/index.tsx
@@ -0,0 +1,21 @@
+/** @jsx h */
+import { h } from "../client_deps.ts";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Satya Rohith G</h1>
+      <p>
+        Hello 👋, I'm Satya. I work for{" "}
+        <a href="https://deno.com">the Deno company</a>.
+      </p>
+      <p>
+        I'm trying to put this website together in my free time.
+      </p>
+      <p>
+        I don't have ideas on what I should blog about. You can{" "}
+        <a href="mailto:me@satyarohith.com">send</a> me your thoughts.
+      </p>
+    </main>
+  );
+}
